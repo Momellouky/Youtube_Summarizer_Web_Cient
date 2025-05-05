43b6310fce22dc11cc042e53b95acbcf3068fa39
wannatry <contact.mellouky@gmail.com>
2025-05-05 17:42:28 +0200

    feat: add MainContent Component
    The MainContentComponent is the orchestrator of the components to be
    dislayed on the page

---
+++ web_client/web_client/src/Components/MainContentComponent/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MakeSearchWarning from '../MakeSearchWarning/MakeSearchWarning'
+import VideoResult from '../VideoResultComponent/VideoResult'
+
+export default function MainContent() {
+  return (
+    <>
+    
+        <VideoResult />
+    
+    </>
+  )
+}
