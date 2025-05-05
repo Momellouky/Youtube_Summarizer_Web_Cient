ba196960c18db1ef589d751124162a9428e4737e
wannatry <contact.mellouky@gmail.com>
2025-05-05 17:40:36 +0200

    feat: add the make search warning component
    The MakeSearchWarning component is displayed to the user when it has to
    make a search in order to get some results.
    It is displayed when it enters the web application and when it reloads
    the web page

---
+++ web_client/web_client/src/Components/MakeSearchWarning/MakeSearchWarning.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../Styles/MakeSearchWarning.css'
+export default function MakeSearchWarning() {
+  return (
+    <>
+    
+        <div className='make-search-warning-container'>
+            <div className='make-search-warning'>
+              <span className='warning-icon'>⚠️</span>
+              <span className='warning-text'>Please make a search to see results</span>
+            </div>
+        </div>
+    
+    </>
+  )
+}
